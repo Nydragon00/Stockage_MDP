c959015f02656e1ff11328c2d9ae610312d0dc62
Nydragon00 <6582485n@gmail.com>
2019-11-26 11:44:33 +0100

    cryptage

--- encrypt_pwd.py
+++ encrypt_pwd.py
@@ -1,16 +1,25 @@
 import csv
 import os
+import math
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
-pwd_tag = input("Enter your password name: ")
-pwd = input("Enter your password: ")
-
-pwd_val = [pwd_tag, pwd]
-
 dir = dir_path + '\\password.csv'
-print (dir)
 
-with open(dir, mode='a') as csv_pwd_db:
+with open(dir, mode='wr') as csv_pwd_db:
+    db_read = csv_pwd_db.read()
+    pwd_tag = input("Enter your password name: ")
+    pwd = input("Enter your password: ")
+    for x in db_read:
+        print(x)
+    pwd = [ord(c) for c in pwd]
+
+    for x in range(0, len(pwd)):
+        # pwd[x] = pwd[x] ** 2 - pwd[x]
+        pwd[x] = (pwd[x] ** 3.141592653589793) / 111000
+        print (pwd[x])
+        # pwd[x] = pwd[x] / 3.141592653589793
+        # pwd[x] = round(pwd[x])
+
+    pwd_val = [pwd_tag, pwd]
     writer = csv.writer(csv_pwd_db)
     writer.writerow(pwd_val)
