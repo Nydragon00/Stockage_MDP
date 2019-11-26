cb0d6716e22aade9be3eab103947332dfa9b6375
Nydragon00 <6582485n@gmail.com>
2019-11-26 15:57:26 +0100

    split write and read into two

--- encrypt_pwd.py
+++ encrypt_pwd.py
@@ -1,18 +1,28 @@
 import csv
 import os
 import math
+import sys
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir = dir_path + '\\password.csv'
 
-with open(dir, mode='wr') as csv_pwd_db:
+with open(dir, mode='r') as csv_pwd_db:
     db_read = csv_pwd_db.read()
-    pwd_tag = input("Enter your password name: ")
-    pwd = input("Enter your password: ")
-    for x in db_read:
-        print(x)
-    pwd = [ord(c) for c in pwd]
+    bool = True
+    while bool:
+        pwd_tag = input("Enter your password name: ")
+        pwd = input("Enter your password: ")
+        for x in db_read:
+            if pwd_tag in db_read:
+                print ("This name is already registered.")
+                break
+            else:
+                bool = False
+    # for x in db_read.split(","):
+    #     print(x)
 
+with open(dir, mode='a') as csv_pwd_db:
+    pwd = [ord(c) for c in pwd]
     for x in range(0, len(pwd)):
         # pwd[x] = pwd[x] ** 2 - pwd[x]
         pwd[x] = (pwd[x] ** 3.141592653589793) / 111000
