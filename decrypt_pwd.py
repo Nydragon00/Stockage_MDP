d6b4c17d8ee963d7cd84e796491ba9974db1b856
Stealian <52290299+Stealian@users.noreply.github.com>
2019-11-27 14:45:57 +0100

    change decrypt

--- decrypt_pwd.py
+++ decrypt_pwd.py
@@ -2,7 +2,8 @@
 import csv
 import os
 import encrypt_pwd
-
+from math import sqrt
+	
 #chemin
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir = dir_path + '\\password.csv'
@@ -13,5 +14,5 @@ with open(dir, mode='r') as csv_pwd_db:
 
 # déchiffrer
 	for x in range(0, len(pwd)):
-		pwd[x] = (pwd[x] / 3.141592653589793) * 111000
+		pwd[x] = (pwd[x] / math.sqrt(3.141592653589793)) * 111000 / (x + 1)
 		print (pwd[x])
